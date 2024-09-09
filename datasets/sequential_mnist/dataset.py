4ab748a52af401c4a533ed30b524dc32f861ce05
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-09-09 17:50:44 +0200

    minor changes

--- datasets/sequential_mnist/dataset.py
+++ datasets/sequential_mnist/dataset.py
@@ -5,7 +5,7 @@ from torch.utils.data import Dataset
 
 class SequentialMNIST(Dataset):
 
-    def __init__(self, training: bool = True):
+    def __init__(self, training: bool = True, normalize: bool = True):
         data = np.load('datasets/sequential_mnist/sequential_mnist_dataset/sequential_mnist.npz')
 
         if training:
@@ -14,6 +14,10 @@ class SequentialMNIST(Dataset):
         else:
             x = data['x_test']
             y = data['y_test']
+
+        if normalize:
+            x = x.astype(np.float32)
+            x /= 255.0
 
         self.data = torch.tensor(x, dtype=torch.float32)
         self.targets = torch.tensor(y, dtype=torch.uint8)
