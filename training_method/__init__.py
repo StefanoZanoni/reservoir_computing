511a1695374f9d96478b0295ad15c8ca88e67b7a
StefanoZanoni <stefano.zanoni00@gmail.com>
2024-09-09 11:39:42 +0200

    removed training methods

--- training_method/__init__.py
+++
@@ -1,5 +0,0 @@
-from training_method.tm import TrainingMethod
-from training_method.ridge import RidgeRegression
-from training_method.least_mean_square import LSM
-
-__all__ = ['TrainingMethod', 'RidgeRegression', 'LSM']
