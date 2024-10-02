added fixed inputs kernel with random signs

reservoir_memory_network/deep_rmn.py
@@ -36,6 +36,7 @@ class DeepReservoirMemoryNetwork(torch.nn.Module):
                  bias: bool = True,
                  bias_scaling: float = None,
                  distribution: str = 'uniform',
+                 signs_from: str = 'pi',
                  non_linearity: str = 'tanh',
                  effective_rescaling: bool = True,
                  concatenate_non_linear: bool = False,
@@ -101,6 +102,7 @@ class DeepReservoirMemoryNetwork(torch.nn.Module):
                                    bias=bias,
                                    bias_scaling=bias_scaling,
                                    distribution=distribution,
+                                   signs_from=signs_from,
                                    non_linearity=non_linearity,
                                    effective_rescaling=effective_rescaling,
                                    circular_non_linear_kernel=circular_non_linear_kernel,
@@ -138,6 +140,7 @@ class DeepReservoirMemoryNetwork(torch.nn.Module):
                                        bias=bias,
                                        bias_scaling=bias_scaling,
                                        distribution=distribution,
+                                       signs_from=signs_from,
                                        non_linearity=non_linearity,
                                        effective_rescaling=effective_rescaling,
                                        circular_non_linear_kernel=circular_non_linear_kernel,
@@ -205,7 +208,7 @@ class DeepReservoirMemoryNetwork(torch.nn.Module):
         states, ys = [], []
         for x, y in tqdm(data, desc='Fitting', disable=disable_progress_bar):
             x, y = x.to(device), y.to(device)
-            state = self(x)[3 if self.just_memory else 1][-1] if use_last_state\
+            state = self(x)[3 if self.just_memory else 1][-1] if use_last_state \
                 else self(x)[2 if self.just_memory else 0]
             states.append(state.cpu().numpy())
             ys.append(y.cpu().numpy())

reservoir_memory_network/rmn.py
@@ -58,6 +58,7 @@ class RMNCell(torch.nn.Module):
                  bias: bool = True,
                  bias_scaling: float = None,
                  distribution: str = 'uniform',
+                 signs_from: str = 'pi',
                  non_linearity: str = 'tanh',
                  effective_rescaling: bool = True,
                  circular_non_linear_kernel: bool = False,
@@ -82,7 +83,7 @@ class RMNCell(torch.nn.Module):
 
         # Input to memory reservoir kernel
         self.input_memory_kernel = init_input_kernel(input_units, memory_units, input_memory_connectivity,
-                                                     input_memory_scaling, distribution)
+                                                     input_memory_scaling, 'fixed', signs_from=signs_from)
         # Memory reservoir kernel
         self.memory_kernel = init_memory_kernel(memory_units, theta, legendre, memory_scaling)
         self._memory_state = None
@@ -199,6 +200,7 @@ class ReservoirMemoryNetwork(torch.nn.Module):
                  bias: bool = True,
                  bias_scaling: float = None,
                  distribution: str = 'uniform',
+                 signs_from: str = 'pi',
                  non_linearity: str = 'tanh',
                  effective_rescaling: bool = True,
                  circular_non_linear_kernel: bool = False,
@@ -225,8 +227,8 @@ class ReservoirMemoryNetwork(torch.nn.Module):
                            input_non_linear_connectivity=input_non_linear_connectivity,
                            non_linear_connectivity=non_linear_connectivity,
                            memory_non_linear_connectivity=memory_non_linear_connectivity, bias=bias,
-                           bias_scaling=bias_scaling, distribution=distribution, non_linearity=non_linearity,
-                           effective_rescaling=effective_rescaling,
+                           bias_scaling=bias_scaling, distribution=distribution, signs_from=signs_from,
+                           non_linearity=non_linearity, effective_rescaling=effective_rescaling,
                            circular_non_linear_kernel=circular_non_linear_kernel, euler=euler, epsilon=epsilon,
                            gamma=gamma, legendre=legendre, theta=theta, just_memory=just_memory)
         if task == 'classification':

test_memory_capacity.py
@@ -11,6 +11,7 @@ def test_rmn():
     spectral_radius = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.999]
     leaky_rates = np.arange(0.1, 1.1, 0.1)
     distributions = ['uniform', 'normal', 'fixed']
+    signs_from = ['pi', 'e', 'logistic', 'random']
     alphas = [1e-3, 1e-2, 1e-1, 1]
     effective_rescaling = [True, False]
     bias = [True, False]
@@ -38,6 +39,7 @@ def test_rmn():
             sr = np.random.choice(spectral_radius)
             lr = np.random.choice(leaky_rates)
             dist = np.random.choice(distributions)
+            signs = np.random.choice(signs_from)
             alpha = np.random.choice(alphas)
             er = np.random.choice(effective_rescaling)
             b = np.random.choice(bias)
@@ -88,6 +90,7 @@ def test_rmn():
                 '--spectral_radius', str(sr),
                 '--leaky_rate', str(lr),
                 '--distribution', dist,
+                '--signs_from', signs,
                 '--non_linearity', 'identity',
                 '--alpha', str(alpha),
                 '--max_iter', '2000',
@@ -133,6 +136,7 @@ def test_rmn():
                 '--spectral_radius', str(sr),
                 '--leaky_rate', str(lr),
                 '--distribution', dist,
+                '--signs_from', signs,
                 '--non_linearity', 'identity',
                 '--alpha', str(alpha),
                 '--max_iter', '2000',
@@ -180,6 +184,7 @@ def test_rmn():
                 '--spectral_radius', str(sr),
                 '--leaky_rate', str(lr),
                 '--distribution', dist,
+                '--signs_from', signs,
                 '--non_linearity', 'identity',
                 '--alpha', str(alpha),
                 '--max_iter', '2000',
@@ -226,6 +231,7 @@ def test_rmn():
                 '--spectral_radius', str(sr),
                 '--leaky_rate', str(lr),
                 '--distribution', dist,
+                '--signs_from', signs,
                 '--non_linearity', 'identity',
                 '--alpha', str(alpha),
                 '--max_iter', '2000',
@@ -327,5 +333,5 @@ def test_esn():
 
 
 if __name__ == '__main__':
-    test_esn()
+    #test_esn()
     test_rmn()

utils/initialization.py
@@ -1,9 +1,9 @@
 import torch
 import numpy as np
+from mpmath import mp
 
 
 def eye_init(M: int) -> torch.FloatTensor:
-
     """
     Generates an identity matrix of size M x M.
 
@@ -18,9 +18,8 @@ def eye_init(M: int) -> torch.FloatTensor:
     return torch.eye(M, dtype=torch.float32)
 
 
-def sparse_tensor_init(M: int, N: int, C: int = 1, distribution: str = 'uniform', scaling: float = 1.0)\
-        -> torch.FloatTensor:
-
+def sparse_tensor_init(M: int, N: int, C: int = 1, distribution: str = 'uniform', scaling: float = 1.0,
+                       signs_from: str | None = 'pi') -> torch.FloatTensor:
     """
     Generates an M x N matrix to be used as sparse kernel.
     For each row only C elements are non-zero (i.e., each input dimension is projected only to C neurons).
@@ -34,6 +33,7 @@ def sparse_tensor_init(M: int, N: int, C: int = 1, distribution: str = 'uniform'
     It can be 'uniform', 'normal' or 'fixed'.
     In the case of normal, the N(0,1) is used, scaled to a variance equal to 1/C.
     :param scaling: Scaling factor for fixed and uniform distribution.
+    :param signs_from: The source of the signs for the fixed distribution.
 
     :return: M x N matrix.
     """
@@ -49,6 +49,32 @@ def sparse_tensor_init(M: int, N: int, C: int = 1, distribution: str = 'uniform'
 
     if distribution == 'fixed':
         values = torch.ones(M * C, dtype=torch.float32) * scaling
+
+        if signs_from is None or signs_from == 'random':
+            # Generate random signs
+            signs = np.random.choice([-1, 1], size=M * C)
+        elif signs_from == 'pi':
+            # Set precision to get enough decimal digits
+            mp.dps = M * C + 2
+
+            digits = str(mp.pi)[2:2 + M * C]
+            signs = np.array([1 if int(d) > 4.5 else -1 for d in digits])
+        elif signs_from == 'e':
+            # Set precision to get enough decimal digits
+            mp.dps = M * C + 2
+
+            digits = str(mp.e)[2:2 + M * C]
+            signs = np.array([1 if int(d) > 4.5 else -1 for d in digits])
+        elif signs_from == 'logistic':
+            # Logistic map initialization
+            x = 0.33
+            signs = []
+            for _ in range(M * C):
+                x = 4 * x * (1 - x)
+                signs.append(1 if x > 0.5 else -1)
+            signs = np.array(signs)
+
+        values *= torch.tensor(signs, dtype=torch.float32)
     elif distribution == 'uniform':
         values = (torch.rand(M * C, dtype=torch.float32) * 2 - 1) * scaling
     elif distribution == 'normal':
@@ -64,7 +90,6 @@ def sparse_tensor_init(M: int, N: int, C: int = 1, distribution: str = 'uniform'
 
 
 def spectral_norm_scaling(W: torch.FloatTensor, rho_desired: float) -> torch.FloatTensor:
-
     """
     Rescales W to have rho(W) = rho_desired
 
@@ -80,7 +105,6 @@ def spectral_norm_scaling(W: torch.FloatTensor, rho_desired: float) -> torch.Flo
 
 
 def fast_spectral_rescaling(W: torch.FloatTensor, rho_desired: float) -> torch.FloatTensor:
-
     """
     Rescales a W uniformly sampled in (-1,1) to have rho(W) = rho_desired.
     This method is fast since we don't need to compute the spectrum of W, which is very slow.
@@ -100,7 +124,6 @@ def fast_spectral_rescaling(W: torch.FloatTensor, rho_desired: float) -> torch.F
 
 
 def circular_tensor_init(M: int, distribution: str = 'uniform', scaling: float = 1.0) -> torch.FloatTensor:
-
     """
     Generates an M x M matrix with ring topology.
     Each neuron receives input only from one neuron and propagates its activation only to one other neuron.
@@ -136,7 +159,6 @@ def circular_tensor_init(M: int, distribution: str = 'uniform', scaling: float =
 
 
 def skewsymmetric(M: int, scaling: float = 1.0) -> torch.FloatTensor:
-
     """
     Generate a skewsymmetric matrix.
 
@@ -170,18 +192,18 @@ def legendre_tensor_init(M: int, theta: float) -> torch.FloatTensor:
     return torch.sparse_coo_tensor(indices.T, values, (M, M), dtype=torch.float32).to_dense()
 
 
-def init_bias(bias, non_linear_units, input_scaling, bias_scaling):
+def init_bias(bias: bool, non_linear_units: int, input_scaling: float, bias_scaling: float) -> torch.FloatTensor:
     if bias:
         bias_scaling = input_scaling if bias_scaling is None else bias_scaling
-        tensor = (2 * torch.rand(non_linear_units) - 1) * bias_scaling
+        tensor = (2 * torch.rand(non_linear_units, dtype=torch.float32) - 1) * bias_scaling
         tensor = tensor.to_dense()
     else:
-        tensor = torch.zeros(non_linear_units).to_dense()
+        tensor = torch.zeros(non_linear_units, dtype=torch.float32).to_dense()
     return torch.nn.Parameter(tensor, requires_grad=False)
 
 
-def rescale_kernel(W, spectral_radius, leaky_rate, effective_rescaling, distribution, non_linear_connectivity,
-                   non_linear_units):
+def rescale_kernel(W: torch.FloatTensor, spectral_radius: float, leaky_rate: float, effective_rescaling: bool,
+                   distribution: str, non_linear_connectivity: int, non_linear_units: int) -> torch.FloatTensor:
     if effective_rescaling and leaky_rate != 1:
         I = eye_init(non_linear_units)
         W = W * leaky_rate + (I * (1 - leaky_rate))
@@ -194,8 +216,11 @@ def rescale_kernel(W, spectral_radius, leaky_rate, effective_rescaling, distribu
     return spectral_norm_scaling(W, spectral_radius)
 
 
-def init_non_linear_kernel(non_linear_units, non_linear_connectivity, distribution, spectral_radius, leaky_rate,
-                           effective_rescaling, circular_non_linear_kernel, euler, gamma, non_linear_scaling):
+def init_non_linear_kernel(non_linear_units: int, non_linear_connectivity: int, distribution: str,
+                           spectral_radius: float, leaky_rate: float, effective_rescaling: bool,
+                           circular_non_linear_kernel: bool, euler: bool, gamma: float, non_linear_scaling: float) \
+        -> torch.FloatTensor:
+
     if euler:
         W = skewsymmetric(non_linear_units, non_linear_scaling)
         kernel = W - gamma * eye_init(non_linear_units)
@@ -211,13 +236,14 @@ def init_non_linear_kernel(non_linear_units, non_linear_connectivity, distributi
     return torch.nn.Parameter(kernel, requires_grad=False)
 
 
-def init_input_kernel(input_units, units, input_connectivity, input_scaling, distribution):
-    kernel = sparse_tensor_init(input_units, units, C=input_connectivity, scaling=input_scaling,
+def init_input_kernel(input_units: int, units: int, input_connectivity: int, input_scaling: float, distribution: str,
+                      signs_from: str | None = None) -> torch.FloatTensor:
+    kernel = sparse_tensor_init(input_units, units, C=input_connectivity, scaling=input_scaling, signs_from=signs_from,
                                 distribution=distribution)
     return torch.nn.Parameter(kernel, requires_grad=False)
 
 
-def init_memory_kernel(memory_units, theta, legendre, scaling):
+def init_memory_kernel(memory_units: int, theta: float, legendre: bool, scaling: float) -> torch.FloatTensor:
     if legendre:
         M = legendre_tensor_init(memory_units, theta)
         return torch.nn.Parameter(torch.matrix_exp(M), requires_grad=False)